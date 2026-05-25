d340f61bd7c88c987fe90b586f52877fb552478c
Diego Morelo <diegomorelohenao@gmail.com>
2026-05-25 17:54:04 -0500

    feat: Actualizar contenido de campaña

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.getElementById("btn").onclick = function () {
     "¡Gracias por unirte a la campaña del Candidato CETRE🎉, has hecho una gran elección!",
   );
 };
+document.getElementById("sp").onclick = function () {
+  alert(
+    "¡Gracias por apoyar la campaña de CETRE, juntos podemos lograr el cambio!",
+  );
+};
